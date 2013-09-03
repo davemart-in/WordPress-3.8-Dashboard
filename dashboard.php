7c55a970e0d9bf7d66f21f2cb7faed494bb80986
dbernar1 <dbernar1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-03 15:33:19 +0000

    Removed duplication of module names from the main plugin file
    git-svn-id: https://plugins.svn.wordpress.org/dashboard/trunk@766348 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dashboard.php
+++ dashboard.php
@@ -14,6 +14,12 @@ if ( !class_exists( 'Plugin_Dashboard' ) ) {
 		const version = '0.1';
 		static $instance;
 		private $screen;
+
+		private $modules = array(
+			'combinednews',
+			'quickdraft',
+			'rightnow',
+		);
 		
 		function __construct() {		
 			self::$instance = $this;
@@ -21,23 +27,19 @@ if ( !class_exists( 'Plugin_Dashboard' ) ) {
 			add_action( 'admin_menu', array( $this , 'dash_add_menu' ) );
 			add_action( 'admin_enqueue_scripts', array( $this , 'enqueue_scripts' ) );
 			
-			include( plugin_dir_path( __FILE__ ) . 'combinednews.php' );
-			include( plugin_dir_path( __FILE__ ) . 'quickdraft.php' );
-			include( plugin_dir_path( __FILE__ ) . 'rightnow.php' );
+			foreach ( $this->modules as $module_slug ) include( plugin_dir_path( __FILE__ ) . $module_slug . '.php';
 		}
 		
 		function enqueue_scripts() {
 			if ( get_current_screen()->base !== $this->screen )
 				return;
 
-			// JS
-			wp_enqueue_script( 'combinednews-js', dirname(__FILE__) . '/js/combinednews.js', array( 'jquery' ), self::version, true );
-			wp_enqueue_script( 'quickdraft-js', dirname(__FILE__) . '/js/quickdraft.js', array( 'jquery' ), self::version, true );
-			wp_enqueue_script( 'rightnow-js', dirname(__FILE__) . '/js/rightnow.js', array( 'jquery' ), self::version, true );
-			// CSS
-			wp_enqueue_style( 'combinednews-css', dirname(__FILE__) . '/css/combinednews.css', array(), self::version );
-			wp_enqueue_style( 'quickdraft-css', dirname(__FILE__) . '/css/quickdraft.css', array(), self::version );
-			wp_enqueue_style( 'rightnow-css', dirname(__FILE__) . '/css/rightnow.css', array(), self::version );
+			foreach ( $this->modules as $module_slug ) {
+				// JS
+				wp_enqueue_script( $module_slug . '-js', dirname(__FILE__) . '/js/' . $module_slug . '.js', array( 'jquery' ), self::version, true );
+				// CSS
+				wp_enqueue_style( $module_slug . '-css', dirname(__FILE__) . '/css/' . $module_slug . '.css', array(), self::version );
+			}
 		}
 		
 		function dash_add_menu() {
