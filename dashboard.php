2de7800f4c69b73ee9b58a332262ad5cc7135f8a
lessbloat <lessbloat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-03 13:11:05 +0000

    Initial base scaffold for core dashboard plugin.
    git-svn-id: https://plugins.svn.wordpress.org/dashboard/trunk@766285 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dashboard.php
+++ dashboard.php
@@ -1,26 +1,60 @@
 <?php
-/*
-Plugin Name: Dashboard Redux
-Plugin URI: http://wordpress.org/plugins/dashboard
-Description: Feature dev plugin for an action-oriented dashboard. Potentially a failure.
-Author: Helen Hou-Sandi
-Version: 0.1
-Author URI: http://profiles.wordpress.org/helen/
-*/
+/* Plugin Name: Dashboard
+ * Description: Refresh WordPress dashboard screen
+ * Author: Core Volunteers
+ * Author URI: http://make.wordpress.org/ui/tag/dash/
+ * Version: 0.1
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-function dddd_admin_menu() {
-	add_dashboard_page( 'Dashboard Redux', 'Dashboard Redux', 'read', 'dddd-dashboard-redux', 'dddd_admin_page' );
-}
-add_action( 'admin_menu', 'dddd_admin_menu' );
+if ( !class_exists( 'Plugin_Dashboard' ) ) {
+	class Plugin_Dashboard {
+		
+		const version = '0.1';
+		static $instance;
+		private $screen;
+		
+		function __construct() {		
+			self::$instance = $this;
+			
+			add_action( 'admin_menu', array( $this , 'dash_add_menu' ) );
+			add_action( 'admin_enqueue_scripts', array( $this , 'enqueue_scripts' ) );
+			
+			include( plugin_dir_path( __FILE__ ) . 'combinednews.php' );
+			include( plugin_dir_path( __FILE__ ) . 'quickdraft.php' );
+			include( plugin_dir_path( __FILE__ ) . 'rightnow.php' );
+		}
+		
+		function enqueue_scripts() {
+			if ( get_current_screen()->base !== $this->screen )
+				return;
 
-function dddd_admin_page() {
-?>
-<div class="wrap">
+			// JS
+			wp_enqueue_script( 'combinednews-js', dirname(__FILE__) . '/js/combinednews.js', array( 'jquery' ), self::version, true );
+			wp_enqueue_script( 'quickdraft-js', dirname(__FILE__) . '/js/quickdraft.js', array( 'jquery' ), self::version, true );
+			wp_enqueue_script( 'rightnow-js', dirname(__FILE__) . '/js/rightnow.js', array( 'jquery' ), self::version, true );
+			// CSS
+			wp_enqueue_style( 'combinednews-css', dirname(__FILE__) . '/css/combinednews.css', array(), self::version );
+			wp_enqueue_style( 'quickdraft-css', dirname(__FILE__) . '/css/quickdraft.css', array(), self::version );
+			wp_enqueue_style( 'rightnow-css', dirname(__FILE__) . '/css/rightnow.css', array(), self::version );
+		}
+		
+		function dash_add_menu() {
+			$this->screen = add_dashboard_page( 'Dash', 'Dash', 'read', 'dash-dash', array( __CLASS__, 'dash_page' ) );
+		}
+		
+		function dash_page() {
+		?>
+		<div class="wrap">
 
-	<?php screen_icon(); ?>
+			<?php screen_icon(); ?>
 
-	<h2><?php esc_html_e( 'Dashboard Redux', 'dddd' ); ?></h2>
+			<h2><?php esc_html_e( 'Dash' ); ?></h2>
 
-</div><!-- .wrap -->
-<?php
+		</div><!-- .wrap -->
+		<?php
+		}
+	}
+	new Plugin_Dashboard;
 }
--- readme.txt
+++ readme.txt
@@ -1,17 +1,18 @@
-=== D ===
-Contributors: helen, ryelle, melchoyce, sillybean
-Tags: feature-dev
-Requires at least: trunk
-Tested up to: trunk
+=== Dashboard ===
+Contributors: lessbloat, joen, helen, dabar, kraftbj
+Tags: dashboard, right now, dashboard widgets
+Requires at least: 3.7
+Tested up to: 3.7
 Stable tag: trunk
-
-Feature dev plugin for an action-oriented dashboard. Potentially a failure.
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
 == Description ==
 
-Sometimes some of us wish the dashboard were more action-oriented. Wishing doesn't do anything, but dev-ing a feature in a plugin does.
+Refresh WordPress dashboard screen.  Clean things up.  
+Move a few things around.  Make it more actionable and relevant for users.
 
 == Changelog ==
 
-= 0.1=
-* Skeleton
+= 0.1 =
+* First version Aug 2013
