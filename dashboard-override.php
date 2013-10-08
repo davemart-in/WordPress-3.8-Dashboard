17ca453282fac204245401a9e18294fc87c047fd
lessbloat <lessbloat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-08 14:47:09 +0000

    Temporarily reenabling columns screen option - will take it back out once the responsive columns CSS is in.
    git-svn-id: https://plugins.svn.wordpress.org/dashboard/trunk@784577 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- dashboard-override.php
+++ dashboard-override.php
@@ -32,6 +32,11 @@ if ( wp_is_mobile() )
 	wp_enqueue_script( 'jquery-touch-punch' );
 
 $parent_file = 'index.php';
+
+if ( is_user_admin() ) 
+	add_screen_option('layout_columns', array('max' => 4, 'default' => 1) ); 
+else 
+	add_screen_option('layout_columns', array('max' => 4, 'default' => 2) ); 
  
 $help = '<p>' . __( 'Welcome to your WordPress Dashboard! This is the screen you will see when you log in to your site, and gives you access to all the site management features of WordPress. You can get help for any screen by clicking the Help tab in the upper corner.' ) . '</p>';
  
